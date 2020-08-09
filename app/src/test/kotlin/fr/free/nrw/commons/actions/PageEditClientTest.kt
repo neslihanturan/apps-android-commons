6980f4164565619ee185de84084cdb5654cb3b71
Ashish <ashishkumar468@gmail.com>
2020-08-09 15:36:41 +0530

    Fixed testAppendEdit test in PageEditClientTest

--- app/src/test/kotlin/fr/free/nrw/commons/actions/PageEditClientTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/actions/PageEditClientTest.kt
@@ -2,13 +2,16 @@ package fr.free.nrw.commons.actions
 
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.verify
+import io.reactivex.Observable
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.wikipedia.csrf.CsrfTokenClient
 import org.wikipedia.dataclient.Service
+import org.wikipedia.edit.Edit
 
 class PageEditClientTest {
     @Mock
@@ -17,6 +20,13 @@ class PageEditClientTest {
     private lateinit var pageEditInterface: PageEditInterface
     @Mock
     private lateinit var service: Service
+
+    @Mock
+    lateinit var edit: Edit
+
+    @Mock
+    lateinit var editResult: Edit.Result
+
 
     private lateinit var pageEditClient: PageEditClient
 
@@ -46,8 +56,23 @@ class PageEditClientTest {
     @Test
     fun testAppendEdit() {
         Mockito.`when`(csrfTokenClient.tokenBlocking).thenReturn("test")
-        pageEditClient.appendEdit("test", "test", "test")
+        Mockito.`when`(
+            pageEditInterface.postAppendEdit(
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString()
+            )
+        ).thenReturn(
+            Observable.just(edit)
+        )
+        Mockito.`when`(edit.edit()).thenReturn(editResult)
+        Mockito.`when`(editResult.editSucceeded()).thenReturn(true)
+        pageEditClient.appendEdit("test", "test", "test").test()
+        verify(csrfTokenClient).tokenBlocking
         verify(pageEditInterface).postAppendEdit(eq("test"), eq("test"), eq("test"), eq("test"))
+        verify(edit).edit()
+        verify(editResult).editSucceeded()
     }
 
     /**
